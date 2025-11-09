96cc28cc99ac0899608043bce6c288310c51b2d9
irrynka <irinakorcinska7@gmail.com>
2025-11-09 19:17:46 +0100

    Models Update2

--- school/admin.py
+++ school/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Subject, Teacher, Class, Student
 
 # Register your models here.
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('name', 'subject')
+
+@admin.register(Class)
+class ClassAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'class_group')
--- school/models.py
+++ school/models.py
@@ -2,13 +2,31 @@ from django.db import models
 
 # Create your models here.
 class Subject(models.Model):
-    pass
+    name = models.CharField(max_length=100, default="Unnamed Subject")
+
+    def __str__(self):
+        return self.name
 
 class Teacher(models.Model):
-    pass
+    name = models.CharField(max_length=100, default="Unnamed Teacher")
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        if self.subject:
+            return f"{self.name} ({self.subject.name})"
+        return self.name
 
 class Class(models.Model):
-    pass
+    name = models.CharField(max_length=50, default="Unnamed Class")
+
+    def __str__(self):
+        return self.name
 
 class Student(models.Model):
-    pass
+    name = models.CharField(max_length=100, default="Unnamed Student")
+    class_group = models.ForeignKey(Class, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        if self.class_group:
+            return f"{self.name} ({self.class_group.name})"
+        return self.name
