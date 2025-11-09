52b93140f029e25f383ffdf1a838b5a20cc4af9c
irrynka <irinakorcinska7@gmail.com>
2025-11-09 19:29:12 +0100

    Models Update3

--- school/admin.py
+++ school/admin.py
@@ -1,20 +1,7 @@
 from django.contrib import admin
 from .models import Subject, Teacher, Class, Student
 
-# Register your models here.
-
-@admin.register(Subject)
-class SubjectAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-
-@admin.register(Teacher)
-class TeacherAdmin(admin.ModelAdmin):
-    list_display = ('name', 'subject')
-
-@admin.register(Class)
-class ClassAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-
-@admin.register(Student)
-class StudentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'class_group')
+admin.site.register(Subject)
+admin.site.register(Teacher)
+admin.site.register(Class)
+admin.site.register(Student)
--- school/models.py
+++ school/models.py
@@ -1,32 +1,27 @@
 from django.db import models
 
-# Create your models here.
 class Subject(models.Model):
-    name = models.CharField(max_length=100, default="Unnamed Subject")
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
 class Teacher(models.Model):
-    name = models.CharField(max_length=100, default="Unnamed Teacher")
-    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, null=True)
+    name = models.CharField(max_length=100)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
 
     def __str__(self):
-        if self.subject:
-            return f"{self.name} ({self.subject.name})"
-        return self.name
+        return f"{self.name} ({self.subject.name})"
 
 class Class(models.Model):
-    name = models.CharField(max_length=50, default="Unnamed Class")
+    name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
 
 class Student(models.Model):
-    name = models.CharField(max_length=100, default="Unnamed Student")
-    class_group = models.ForeignKey(Class, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=100)
+    class_group = models.ForeignKey(Class, on_delete=models.CASCADE)
 
     def __str__(self):
-        if self.class_group:
-            return f"{self.name} ({self.class_group.name})"
-        return self.name
+        return f"{self.name} ({self.class_group.name})"
