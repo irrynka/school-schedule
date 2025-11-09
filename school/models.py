f289f760f3d11287cebfd82f891e1fa891832fed
irrynka <irinakorcinska7@gmail.com>
2025-11-09 18:44:33 +0100

    Models Update

--- school/models.py
+++ school/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Subject(models.Model):
+    pass
+
+class Teacher(models.Model):
+    pass
+
+class Class(models.Model):
+    pass
+
+class Student(models.Model):
+    pass
